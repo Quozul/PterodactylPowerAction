62c5df1b1e3c879b6d36fcb84ee1cbb7238ccd3e
LucernaSancta <tommaso.pattaro@hotmail.com>
2026-07-15 19:05:30 +0200

    Switched from TranslationRegistry to TranslationStore

--- src/main/java/fr/pickaria/pterodactylpoweraction/PterodactylPowerAction.java
+++ src/main/java/fr/pickaria/pterodactylpoweraction/PterodactylPowerAction.java
@@ -14,10 +14,11 @@ import fr.pickaria.pterodactylpoweraction.configuration.ConfigurationLoader;
 import fr.pickaria.pterodactylpoweraction.configuration.ShutdownBehaviour;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.translation.GlobalTranslator;
-import net.kyori.adventure.translation.TranslationRegistry;
+import net.kyori.adventure.translation.TranslationStore;
 import org.slf4j.Logger;
 
 import java.nio.file.Path;
+import java.text.MessageFormat;
 import java.time.Duration;
 import java.util.Locale;
 import java.util.NoSuchElementException;
@@ -75,12 +76,12 @@ public class PterodactylPowerAction {
     }
 
     private void initializeTranslator(ResourceBundle... bundles) {
-        TranslationRegistry registry = TranslationRegistry.create(Key.key("pickaria:power_action"));
+        TranslationStore.StringBased<MessageFormat> store = TranslationStore.messageFormat(Key.key("pickaria:power_action"));
         for (ResourceBundle bundle : bundles) {
-            registry.registerAll(bundle.getLocale(), bundle, true);
+            store.registerAll(bundle.getLocale(), bundle, true);
         }
-        registry.defaultLocale(Locale.ENGLISH);
-        GlobalTranslator.translator().addSource(registry);
+        store.defaultLocale(Locale.ENGLISH);
+        GlobalTranslator.translator().addSource(store);
     }
 
     private void initializeCommand() {
